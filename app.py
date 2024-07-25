352fdffe4f5eedc4e0c0aa6afe29a803d0dc80fc
MattheuJ <mattheujimenez@gmail.com>
2024-07-25 14:45:21 -0700

    Finisehd UI

--- app.py
+++ app.py
@@ -6,5 +6,14 @@ app = Flask(__name__)
 def theCasino():
     return render_template('index.html')
 
+@app.route('/coinflip', methods = ['GET', 'POST'])
+def coinFlip():
+    return render_template('coinFlip.html')
+
+@app.route('/error', methods = ['GET', 'POST'])
+def error():
+    return render_template('error.html')
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
