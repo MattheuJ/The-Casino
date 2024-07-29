7e6153b17e6c5804f7de50ef2cbc8d11b6f83f84
MattheuJ <mattheujimenez@gmail.com>
2024-07-29 14:53:57 -0700

    Added a whole bunch of stuff

--- app.py
+++ app.py
@@ -1,18 +1,40 @@
 from flask import Flask, render_template, request, url_for
+import random
 
 app = Flask(__name__)
 
+Balance = 5000
+
 @app.route('/', methods = ['GET', 'POST'])
-def theCasino():
-    return render_template('index.html')
+def theCasino():   
+    return render_template('index.html', Balance = Balance)
 
 @app.route('/coinflip', methods = ['GET', 'POST'])
 def coinFlip():
-    return render_template('coinFlip.html')
+    coin = ["heads", "tails"]
+    didYouWin = 'no!'
+    winningSide = ""
+    headsOrTails = ""
+
+    if request.method == 'POST':
+        headsOrTails = request.form['headsOrTails']
+        betAmount = request.form['betAmount']
+        winningSide = random.choice(coin)
+
+        if betAmount > Balance:
+            errorMessage = "Inusuffciant balance"
+
+        if winningSide == headsOrTails:
+            didYouWin = "yes!"
+        else:
+            didYouWin = "no!"
+        
+    return render_template('coinFlip.html', url=url_for('coinFlip'), didYouWin = didYouWin, winningSide = winningSide, headsOrTails = headsOrTails, Balance = Balance)
 
 @app.route('/error', methods = ['GET', 'POST'])
-def error():
-    return render_template('error.html')
+def error(): 
+
+    return render_template('error.html', Balance = Balance)
 
 
 if __name__ == '__main__':
