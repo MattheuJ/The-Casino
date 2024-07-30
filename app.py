f37c30c46b796a70ae04801451ac0ae3ff0604b9
MattheuJ <mattheujimenez@gmail.com>
2024-07-30 08:59:21 -0700

    Balance now working

--- app.py
+++ app.py
@@ -1,16 +1,23 @@
 from flask import Flask, render_template, request, url_for
 import random
+import json
 
 app = Flask(__name__)
 
-Balance = 5000
+with open('something.json', 'r') as file:
+    balanceData = json.load(file)
+    balance = balanceData["balance"]
 
 @app.route('/', methods = ['GET', 'POST'])
 def theCasino():   
-    return render_template('index.html', Balance = Balance)
+    return render_template('index.html', balance = balance)
 
 @app.route('/coinflip', methods = ['GET', 'POST'])
 def coinFlip():
+
+    from app import balance
+    from app import balanceData
+
     coin = ["heads", "tails"]
     didYouWin = 'no!'
     winningSide = ""
@@ -18,23 +25,48 @@ def coinFlip():
 
     if request.method == 'POST':
         headsOrTails = request.form['headsOrTails']
-        betAmount = request.form['betAmount']
+        betAmount = int(request.form['betAmount'])
         winningSide = random.choice(coin)
 
-        if betAmount > Balance:
-            errorMessage = "Inusuffciant balance"
+        with open("something.json") as file:
+            balanceData = json.load(file)
+            balanceData["betAmount"] = betAmount
 
-        if winningSide == headsOrTails:
-            didYouWin = "yes!"
-        else:
-            didYouWin = "no!"
+        with open('something.json', 'w') as file:
+                json.dump(balanceData,  file, indent=4)
         
-    return render_template('coinFlip.html', url=url_for('coinFlip'), didYouWin = didYouWin, winningSide = winningSide, headsOrTails = headsOrTails, Balance = Balance)
+
+        if betAmount > balance:
+            didYouWin = "Insuficiant balance!"
+        elif betAmount < 0:
+            didYouWin = "No neagtive valiues!"
+        elif winningSide == headsOrTails:
+            didYouWin = "yes!"
+            with open('something.json') as file:
+                balanceData = json.load(file)
+                balanceData["balance"] = balance + betAmount
+            
+            with open('something.json', 'w') as file:
+                json.dump(balanceData,  file, indent=4)
+
+        elif winningSide != headsOrTails:
+            didYouWin = "no!"
+            with open('something.json') as file:
+                balanceData = json.load(file)
+                balanceData["balance"] = balance - betAmount
+            
+            with open('something.json', 'w') as file:
+                json.dump(balanceData,  file, indent=4)
+        
+        balance = balanceData["balance"]
+            
+        
+    return render_template('coinFlip.html', url=url_for('coinFlip'), didYouWin = didYouWin, winningSide = winningSide, headsOrTails = headsOrTails, balance = balance)
 
 @app.route('/error', methods = ['GET', 'POST'])
 def error(): 
 
-    return render_template('error.html', Balance = Balance)
+    return render_template('error.html', balance = balance)
 
 
 if __name__ == '__main__':
